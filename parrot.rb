87f8a64a1e33067754a5451accade1946495b1d8
mostafa qanbar <mustafa.qanbar@yahoo.com>
2020-10-05 07:18:29 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (bird="Parrot", sound="Squawk!")
+def parrot(bird="Parrot", sound="Squawk!")
   puts "the #{bird} makes the #{sound} sound"
 end
